10835d83afd301cebfb6b5c76f325254a6bf6ea2
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-11-25 19:08:42 -0500

    Initial import for new direction of the project.

---
+++ tests/library/ErrorTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This file is part of the slim-skeleton-app package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace RelengTest;
+
+
+class ErrorTest
+{
+
+} 
