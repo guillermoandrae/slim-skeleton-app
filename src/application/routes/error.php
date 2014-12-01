a560fe232ea83b620bba404d3fce7cd7e7ab530b
Guillermo A. Fisher <guillermoandraefisher@gmail.com>
2014-12-01 03:51:00 -0500

    Fixed the starter templates.

--- src/application/routes/error.php
+++ src/application/routes/error.php
@@ -2,8 +2,15 @@
 
 $app->error(function (\Exception $e) use ($app) {
     error_log(sprintf('%s on line %d of %s', $e->getMessage(), $e->getLine(), $e->getFile()));
-    $app->render('layouts/master', array(
+    $app->render('layouts/master.phtml', array(
         'partial' => 'errors/index',
         'error' => $e
+    ));
+});
+
+$app->notFound(function () use ($app) {
+    $app->render('layouts/master.phtml', array(
+        'partial' => 'errors/index',
+        'error' => new \Exception('Page not found')
     ));
 });
